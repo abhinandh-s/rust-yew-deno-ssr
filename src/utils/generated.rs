130d26b376f4da58c836be12d432f7e5f14a75e4
abhi <abhinandhsuby@proton.me>
2026-02-08 13:50:04 +0530

    artice page scroll along with index fix

--- src/utils/generated.rs
+++ src/utils/generated.rs
@@ -1,5 +1,11 @@
 // AUTO-GENERATED — DO NOT EDIT
 
 pub const ARTICLES: &[(&str, &str)] = &[
-   ("hello-world", include_str!("../../articles/published/hello-world.md")),
+   ("hello-world-01", include_str!("../../articles/published/hello-world-01.md")),
+   ("hello-world-02", include_str!("../../articles/published/hello-world-02.md")),
+   ("hello-world-03", include_str!("../../articles/published/hello-world-03.md")),
+   ("hello-world-04", include_str!("../../articles/published/hello-world-04.md")),
+   ("hello-world-05", include_str!("../../articles/published/hello-world-05.md")),
+   ("hello-world-06", include_str!("../../articles/published/hello-world-06.md")),
+   ("hello-world-10", include_str!("../../articles/published/hello-world-10.md")),
 ];
